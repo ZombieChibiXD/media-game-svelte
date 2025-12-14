2435e0e882d07746208d05fb349984c6bff9549c
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2025-12-15 07:50:27 +0800

    Fullscreen button created

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -28,8 +28,26 @@
     });
   });
 
+  let viewport: HTMLDivElement | null = $state(null)
+
   let layoutWidth = $state(0);
   let layoutHeight = $state(0);
+
+  let isFullScreen = $state(false)
+  let fullScreenClicked: (el: Event)=>void = $state(()=>{})
+
+  $effect(()=>{
+    if (!viewport) return;
+    fullScreenClicked = (el) => {
+      if (!document.fullscreenElement) {
+        isFullScreen = true;
+        viewport?.requestFullscreen();
+      } else {
+        document.exitFullscreen?.();
+        isFullScreen = false;
+      }
+    }
+  })
 </script>
 
 {#if !isLandscape}
@@ -37,10 +55,17 @@
 		<p>Rotate your device</p>
 	</div>
 {:else}
-	<div class="viewpot bg-black flex items-center justify-center w-screen h-screen">
+	<div class="viewpot bg-black flex items-center justify-center w-screen h-screen" bind:this={viewport}>
     <div class="game-container">
       <div class="game border border-white text-white relative overflow-hidden" bind:clientWidth={layoutWidth} bind:clientHeight={layoutHeight} style="--view-width: {layoutWidth}px; --view-height: {layoutHeight}px">
         {@render children()}
+        <button onclick={fullScreenClicked} class="bg-gray-300/80 hover:bg-gray-300/90 active:bg-black transition-all duration-300 cursor-pointer absolute bottom-0 right-0 m-5 p-3 rounded-lg">
+          {#if isFullScreen}
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-icon lucide-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>
+          {:else}
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-maximize-icon lucide-maximize"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>
+          {/if}
+        </button>
       </div>
     </div>
 	</div>
