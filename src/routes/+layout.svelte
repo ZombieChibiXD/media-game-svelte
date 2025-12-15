f5203b3301444ff96d19fd799ae4c845e67fed58
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2025-12-16 03:43:32 +0800

    Back button

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-  import { onNavigate } from '$app/navigation';
+  import { afterNavigate, goto, onNavigate } from '$app/navigation';
 	import { onMount } from "svelte";
+	import { base } from '$app/paths';
 
 
 	let { children } = $props();
@@ -48,6 +49,12 @@
       }
     }
   })
+
+  let previousPage: string = $state(base);
+
+  afterNavigate(({from})=>{
+    previousPage = from?.url.pathname || previousPage;
+  })
 </script>
 
 {#if !isLandscape}
@@ -59,6 +66,9 @@
     <div class="game-container">
       <div class="game border border-white text-white relative overflow-hidden" bind:clientWidth={layoutWidth} bind:clientHeight={layoutHeight} style="--view-width: {layoutWidth}px; --view-height: {layoutHeight}px">
         {@render children()}
+        <button onclick={()=>{ goto(previousPage) }} title="Navigate back" class="bg-gray-300/80 hover:bg-gray-300/90 active:bg-black transition-all duration-300 cursor-pointer absolute top-0 left-0 m-[2cqb] p-[2cqb] rounded-lg">
+          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-big-left-icon lucide-arrow-big-left"><path d="M13 9a1 1 0 0 1-1-1V5.061a1 1 0 0 0-1.811-.75l-6.835 6.836a1.207 1.207 0 0 0 0 1.707l6.835 6.835a1 1 0 0 0 1.811-.75V16a1 1 0 0 1 1-1h6a1 1 0 0 0 1-1v-4a1 1 0 0 0-1-1z"/></svg>
+        </button>
         <button onclick={fullScreenClicked} class="bg-gray-300/80 hover:bg-gray-300/90 active:bg-black transition-all duration-300 cursor-pointer absolute bottom-0 right-0 m-[2cqb] p-[2cqb] rounded-lg">
           {#if isFullScreen}
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-minimize-icon lucide-minimize"><path d="M8 3v3a2 2 0 0 1-2 2H3"/><path d="M21 8h-3a2 2 0 0 1-2-2V3"/><path d="M3 16h3a2 2 0 0 1 2 2v3"/><path d="M16 21v-3a2 2 0 0 1 2-2h3"/></svg>
