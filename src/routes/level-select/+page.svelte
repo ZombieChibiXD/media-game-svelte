26e8ec5e8d2139db62d9d4a6299ed0bd0b6a9413
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2025-12-15 08:13:18 +0800

    Created level select and main menu

---
+++ src/routes/level-select/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+	import { cn } from "$lib/utils";
+	import Landing from '../Landing.jpg'
+
+</script>
+
+<img src={Landing} alt="Landing" srcset="" />
+
+<div
+	class={cn(
+		'absolute inset-0 m-auto w-fit h-fit py-2 px-5 flex flex-col',
+		'*:[a]:px-20 *:[a]:py-4 *:[a]:text-2xl *:[a]:uppercase *:[a]:rounded-full',
+		'*:[a]:font-semibold *:[a]:tracking-widest ',
+		'*:[a]:bg-green-600 *:[a]:hover:bg-green-500 *:[a]:active:bg-green-700',
+	)}
+>
+	<a href="/level-select">Play</a>
+</div>
