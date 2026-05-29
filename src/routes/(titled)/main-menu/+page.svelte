1d6957570c90e101c15701f924746c3dea411d48
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2026-05-29 12:40:17 +0800

    add SVGs correctly

--- src/routes/(titled)/main-menu/+page.svelte
+++ src/routes/(titled)/main-menu/+page.svelte
@@ -2,18 +2,25 @@
 	import { cn } from "$lib/utils";
 	import Landing from '$lib/Landing.jpg'
 
+	let playHover = $state(false)
 </script>
 
 <img src={Landing} alt="Landing" srcset="" />
 
 <div
 	class={cn(
-		'absolute inset-0 top-0 bottom-0 m-auto w-fit h-fit py-2 px-5 flex flex-col items-center gap-8',
-		'*:[a]:px-20 *:[a]:py-4 *:[a]:text-2xl *:[a]:uppercase *:[a]:rounded-full',
-		'*:[a]:font-semibold *:[a]:tracking-widest ',
-		'*:[a]:bg-green-600 *:[a]:hover:bg-green-500 *:[a]:active:bg-green-700',
-	)}
+		'absolute inset-0 top-0 bottom-0 m-auto w-fit h-fit py-2 px-5 flex flex-col items-center gap-8'	)}
 >
 	<img src="/logo.png" alt="Logo" class="w-64 h-auto" />
-	<a href="/level-select">Play</a>
+	<a
+		href="/level-select"
+		onmouseenter={() => playHover = true}
+		onmouseleave={() => playHover = false}
+	>
+		<img
+			src={playHover ? '/play_hover.svg' : '/play.svg'}
+			alt="Play"
+			class="w-64 h-auto"
+		/>
+	</a>
 </div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -96,11 +96,11 @@
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M8 3H5a2 2 0 0 0-2 2v3"/><path d="M21 8V5a2 2 0 0 0-2-2h-3"/><path d="M3 16v3a2 2 0 0 0 2 2h3"/><path d="M16 21h3a2 2 0 0 0 2-2v-3"/></svg>
           {/if}
         </button>
-        <button onclick={toggleMusic} class="bg-gray-300/80 hover:bg-gray-300/90 active:bg-black transition-all duration-300 cursor-pointer absolute bottom-0 left-0 m-[2cqb] p-[2cqb] rounded-lg" title={musicOn ? 'Mute music' : 'Play music'}>
+        <button onclick={toggleMusic} class="active:bg-gray-500 transition-all duration-300 cursor-pointer absolute bottom-0 left-0 m-[2cqb] rounded-lg" title={musicOn ? 'Mute music' : 'Play music'}>
           {#if musicOn}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/><path d="M16 9a5 5 0 0 1 0 6"/><path d="M19.364 18.364a9 9 0 0 0 0-12.728"/></svg>
+          <object data="/Unmute.svg" type="image/svg+xml" class="w-[6cqb] h-[6cqb] pointer-events-none">Unmute</object>
           {:else}
-          <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M11 4.702a.705.705 0 0 0-1.203-.498L6.413 7.587A1.4 1.4 0 0 1 5.416 8H3a1 1 0 0 0-1 1v6a1 1 0 0 0 1 1h2.416a1.4 1.4 0 0 1 .997.413l3.383 3.384A.705.705 0 0 0 11 19.298z"/><line x1="22" x2="16" y1="9" y2="15"/><line x1="16" x2="22" y1="9" y2="15"/></svg>
+          <object data="/mute.svg" type="image/svg+xml" class="w-[6cqb] h-[6cqb] pointer-events-none">Mute</object>
           {/if}
         </button>
       </div>
