594ce2e547956226496aacbcfe3e4de792932418
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2026-05-29 12:06:31 +0800

    logo add

--- src/routes/(titled)/main-menu/+page.svelte
+++ src/routes/(titled)/main-menu/+page.svelte
@@ -8,11 +8,12 @@
 
 <div
 	class={cn(
-		'absolute inset-0 top-0 bottom-0 m-auto w-fit h-fit py-2 px-5 flex flex-col',
+		'absolute inset-0 top-0 bottom-0 m-auto w-fit h-fit py-2 px-5 flex flex-col items-center gap-8',
 		'*:[a]:px-20 *:[a]:py-4 *:[a]:text-2xl *:[a]:uppercase *:[a]:rounded-full',
 		'*:[a]:font-semibold *:[a]:tracking-widest ',
 		'*:[a]:bg-green-600 *:[a]:hover:bg-green-500 *:[a]:active:bg-green-700',
 	)}
 >
+	<img src="/logo.png" alt="Logo" class="w-64 h-auto" />
 	<a href="/level-select">Play</a>
 </div>
