75df9137a866954d2af2a4c2b64a377e28e9935a
ZombieChibiXD <43260238+ZombieChibiXD@users.noreply.github.com>
2026-05-29 12:45:57 +0800

    refactor layout and main menu components for improved structure and readability

--- src/routes/(titled)/+layout.svelte
+++ src/routes/(titled)/+layout.svelte
@@ -1,17 +1,7 @@
 <script lang="ts">
-	import { AIFont } from '$lib/components/ai-font';
 
 	let { children } = $props();
-    const scale = 0.5;
-    const gap = 10;
-
 </script>
 
-
-<div class="absolute inset-0 mx-auto py-2 w-fit h-fit flex flex-col items-center justify-center">
-		<AIFont text={'Mari belajar mengenal'} {scale} {gap} />
-		<AIFont text={'hewan bersama di'} {scale} {gap} />
-		<AIFont text={'SD Saraswati 4 Denpasar'} {scale} {gap} />
-</div>
 
 {@render children()}
--- src/routes/(titled)/main-menu/+page.svelte
+++ src/routes/(titled)/main-menu/+page.svelte
@@ -1,26 +1,32 @@
 <script>
 	import { cn } from "$lib/utils";
+	import { AIFont } from '$lib/components/ai-font';
 	import Landing from '$lib/Landing.jpg'
 
 	let playHover = $state(false)
+    const scale = 0.5;
+    const gap = 10;
+
 </script>
 
 <img src={Landing} alt="Landing" srcset="" />
 
 <div
 	class={cn(
-		'absolute inset-0 top-0 bottom-0 m-auto w-fit h-fit py-2 px-5 flex flex-col items-center gap-8'	)}
+		'absolute inset-0 top-0 bottom-0 m-auto w-fit h-fit py-2 px-5 flex flex-col items-center gap-8'
+	)}
 >
-	<img src="/logo.png" alt="Logo" class="w-64 h-auto" />
+	<div class="mx-auto py-2 w-fit h-fit flex flex-col items-center justify-center">
+		<img src="/logo.png" alt="Logo" class="w-64 h-fit" />
+		<AIFont text={'Mari belajar mengenal'} {scale} {gap} />
+		<AIFont text={'hewan bersama di'} {scale} {gap} />
+		<AIFont text={'SD Saraswati 4 Denpasar'} {scale} {gap} />
+	</div>
 	<a
 		href="/level-select"
-		onmouseenter={() => playHover = true}
-		onmouseleave={() => playHover = false}
+		onmouseenter={() => (playHover = true)}
+		onmouseleave={() => (playHover = false)}
 	>
-		<img
-			src={playHover ? '/play_hover.svg' : '/play.svg'}
-			alt="Play"
-			class="w-64 h-auto"
-		/>
+		<img src={playHover ? '/play_hover.svg' : '/play.svg'} alt="Play" class="w-64 h-auto" />
 	</a>
 </div>
